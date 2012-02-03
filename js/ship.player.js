2e0a5a9fb9ff0874124e533699d3454d6ea85b60
Ondrej Zara <ondrej.zara@gmail.com>
2012-02-03 15:55:11 +0100

    preliminary socket support

--- js/ship.player.js
+++ js/ship.player.js
@@ -1,14 +1,28 @@
 /**
- * Player's ship adjusts viewport and has an UI pilot
+ * Player's ship adjusts viewport and has an UI pilot; can send its data to a socket
  */
 Ship.Player = OZ.Class().extend(Ship);
 Ship.Player.prototype.init = function(game) {
 	Ship.prototype.init.call(this, game);
 	this._pilot = new Pilot.UI(this._game, this, "Human pilot");
+
+	this._id = Math.random().toString().replace(/\D/g, "");
+	this._socket = null;
+}
+
+Ship.Player.prototype.setSocket = function(socket) {
+	this._socket = socket;
+	return this;
 }
 
 Ship.Player.prototype.tick = function(dt) {
 	var visualChanged = Ship.prototype.tick.call(this, dt);
+	
+	if (this._socket) { 
+		var data = {};
+		data[this._id] = this._phys.position;
+		this._socket.send(JSON.stringify(data));
+	}
 	
 	var limit = 200;
 	var port = this._game.getPort();
