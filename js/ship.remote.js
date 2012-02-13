dd344a91e64a687d2d0c934b6c627b35355f8764
ondras <none@none>
2012-02-13 15:17:01 +0100

    removed obsolete file

--- js/ship.remote.js
+++
@@ -1,9 +0,0 @@
-Ship.Remote = OZ.Class().extend(Ship);
-Ship.Remote.prototype.init = function(game, position) {
-	Ship.prototype.init.call(this, game, {color:"green", position:position});
-}
-
-Ship.prototype.setPosition = function(position) {
-	this._phys.position = position;
-}
-
