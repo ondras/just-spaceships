828b6dc5e95a7d47f5b57be71af70e05e5284b03
ondras <none@none>
2012-04-26 16:15:28 +0200

    ws
    --HG--
    rename : server/server.js => server/run.js
    rename : server/wrapper.js => server/server.js

---
+++ server/run.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env v8cgi
+
+var Server = require("websocket").Server;
+var ws = new Server(system.args[1] || "0.0.0.0", system.args[2] || 8888);
+
+require("./server").init(ws);
+
+ws.run();
--- server/server.js
+++ server/server.js
@@ -1,8 +1,40 @@
-#!/usr/bin/env v8cgi
+var FS = require("fs");
+var BASE = module.id.split("/");
+BASE.pop();
+BASE.pop();
+BASE = BASE.join("/");
 
-var Server = require("websocket").Server;
-var ws = new Server(system.args[1] || "0.0.0.0", system.args[2] || 8888);
+exports.init = function(ws) {
+	/* mininal DOM environment */
+	var nullElm = {
+		style: {},
+		appendChild: function() {},
+		getContext: function() {}
+	}
+	var window = {};
+	var location = {};
+	var document = {
+		createElement: function() { return nullElm; }
+	};
+	var navigator = {userAgent:""};
+	global.setTimeout = function() {};
 
-require("./wrapper").init(ws);
+	/* read all javascript files */
+	var index = new FS.File(BASE + "/index.html").open("r");
+	var html = index.read().toString("utf-8");
+	index.close();
+	var scripts = html.match(/js\/.*?\.js/g);
+	for (var i=0;i<scripts.length;i++) {
+		var f = new FS.File(BASE + "/"+scripts[i]);
+		system.stdout.writeLine("Loading " + f);
+		try {
+			eval(f.open("r").read().toString("utf-8"));
+		} catch (e) {
+			system.stdout.writeLine(e);
+		}
+	}
+	/* */
 
-ws.run();
+	HAF.Engine.prototype.draw = function() {}
+	new Game.Server(ws).start();
+}
