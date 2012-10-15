cbc8d6fe04a71435dd8e32d77a5d81a45d1e8b6c
Ezeki <ezeki@jok.ge>
2012-10-15 19:01:58 +0400

    fixed: audio problem for webkit browsers. moved to SoundJS

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,20 @@
+(function() {
+	// Load audio files
+	var manifest = [
+				{ id:"shot", src:"sfx/shot.mp3|sfx/shot.ogg", data:20 },
+				{ id:"explosion", src:"sfx/explosion.mp3|sfx/explosion.ogg", data:6 },
+				{ id:"neointro", src:"sfx/neointro.mp3|sfx/neointro.ogg" }
+			];
+
+	preload = new createjs.PreloadJS();
+	preload.installPlugin(createjs.SoundJS);
+	preload.loadManifest(manifest);
+})();
+
+
 Game.Audio = {
-	_supported: !!window.Audio && !(navigator.userAgent.match(/linux/i) && navigator.userAgent.match(/firefox/i)),
 	play: function(name) {
-		if (!this._supported) { return; }
-		var a = new Audio();
-		var ext = (a.canPlayType("audio/ogg") ? "ogg" : "mp3");
-		a.src = "sfx/" + name + "." + ext;
-		a.play();
+		
+		createjs.SoundJS.play(name);
 	}
 }
