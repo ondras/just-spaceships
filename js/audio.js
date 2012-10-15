be8fb6d7001ed73bdec2bb3ff9b7ca472d042530
Ezeki <ezeki@jok.ge>
2012-10-16 00:43:30 +0400

    added: node.js support

--- js/audio.js
+++ js/audio.js
@@ -1,20 +1,5 @@
-(function() {
-	// Load audio files
-	var manifest = [
-				{ id:"shot", src:"sfx/shot.mp3|sfx/shot.ogg", data:20 },
-				{ id:"explosion", src:"sfx/explosion.mp3|sfx/explosion.ogg", data:6 },
-				{ id:"neointro", src:"sfx/neointro.mp3|sfx/neointro.ogg" }
-			];
-
-	preload = new createjs.PreloadJS();
-	preload.installPlugin(createjs.SoundJS);
-	preload.loadManifest(manifest);
-})();
-
-
 Game.Audio = {
 	play: function(name) {
-		
 		createjs.SoundJS.play(name);
 	}
 }
