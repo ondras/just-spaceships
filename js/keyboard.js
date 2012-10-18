705ac6adf70cba607d48fdb9010a8d1f76a95566
Ezeki <ezeki@jok.ge>
2012-10-18 19:16:04 +0400

    added: new maneuver abilities

--- js/keyboard.js
+++ js/keyboard.js
@@ -18,22 +18,35 @@ Keyboard.prototype._keydown = function(e) {
 		case 32:
 			this._control.fire = true;
 		break;
+		case 65: // A
 		case 37:
 			this._control.torque.mode = 2;
 			this._control.torque.target = -1;
 		break;
+		case 68: // D
 		case 39:
 			this._control.torque.mode = 2;
 			this._control.torque.target = +1;
 		break;
 		
-		case 38:
+		case 87: // W
+		case 38: 
 			this._control.engine = 1;
 		break;
+		case 83: // S
 		case 40:
 			this._control.engine = -1;
 		break;
+
+		case 81: // Q
+			this._control.engine2 = -1;
+		break;
+
+		case 69: // E
+			this._control.engine2 = +1;
+		break;
 	}
+
 	/* FIXME jen pri zmene? */
 	this.dispatch("keyboard-change");
 }
@@ -47,16 +60,25 @@ Keyboard.prototype._keyup = function(e) {
 			this._control.fire = false;
 		break;
 		
+		case 65: // A
+		case 68: // D
 		case 37:
 		case 39:
 			this._control.torque.mode = 0;
 		break;
 		
+		case 87: // W
+		case 83: // S
 		case 38:
 		case 40:
 			this._control.engine = 0;
 		break;
-		
+
+
+		case 81: // Q
+		case 69: // E
+			this._control.engine2 = 0;
+		break;
 	}
 	/* FIXME jen pri zmene? */
 	this.dispatch("keyboard-change");
