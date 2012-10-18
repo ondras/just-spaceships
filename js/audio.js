49845eb5fb03533e662bf03c091dc5ce0f5a26e8
Ezeki <ezeki@jok.ge>
2012-10-18 18:22:17 +0400

    limited: max conncurent audios playing

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,17 @@
 Game.Audio = {
+	_maxConcurentAudio: 5,
+	_concurentAudio: 0,
 	_supported: !!window.Audio && !(navigator.userAgent.match(/linux/i) && navigator.userAgent.match(/firefox/i)),
 	play: function(name) {
 		if (!this._supported) { return; }
+		if (this._maxConcurentAudio < this._concurentAudio) { return; }
+
 		var a = new Audio();
 		var ext = (a.canPlayType("audio/ogg") ? "ogg" : "mp3");
 		a.src = "sfx/" + name + "." + ext;
+		a.addEventListener('ended', (function() { this._concurentAudio--; }).bind(this));
 		a.play();
+
+		this._concurentAudio++;
 	}
 }
